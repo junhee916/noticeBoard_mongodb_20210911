29b47ae37a34ed31f3d6b6a5aef9b29ec2eef431
junhee916 <junhee916@naver.com>
2021-09-11 14:28:39 +0900

    setting ejs || signup and login

--- server.js
+++ server.js
@@ -2,11 +2,26 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const path = require('path')
 const app = express()
 
 const userRouter = require('./routes/user')
 const boardRouter = require('./routes/board')
 const commendRouter = require('./routes/commend')
+
+// setting ejs
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+
+    res.render('login')
+})
+
+app.get('/signup', (req, res) => {
+
+    res.render('signup')
+})
 
 // connected mongodb...
 const connectDB = require('./config/database')
@@ -20,6 +35,7 @@ app.use(morgan('dev'))
 
 // connected uploads
 app.use('/uploads', express.static('uploads'))
+app.use('/scripts', express.static('scripts'))
 
 // connected routes
 app.use('/user', userRouter)
